cf1f2ad28a8d6d58e795a1e5acc5c5a9fe18e3ca
Adam <omahonyadam@gmail.com>
2020-02-19 13:17:28 +0000

    change to mapbox dark-v9 tile layer

--- frontend/src/Map/Map.js
+++ frontend/src/Map/Map.js
@@ -4,16 +4,14 @@ import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 import L from 'leaflet'
 
 export default function MapComp() {
-  // const myMap = L.map('mapid', {
-  //   center: [37.7749, -122.4194],
-  //    zoom: 13
-  //  })
+  const mapbox_url =
+   "https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3F1cnJsaSIsImEiOiJjamhxY2lkeTcxOG9jMzdudTZkYm9jbmlkIn0.zvZPOE4XHex2E18F0FSdSg"
 
   const position = [51.505, -0.09]
   const map = (
   <Map center={position} zoom={13}>
     <TileLayer
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      url={mapbox_url}
       attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
     />
     <Marker position={position}>
