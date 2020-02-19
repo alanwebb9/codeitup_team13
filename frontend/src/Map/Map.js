843809d1ba2de7efb0946cdca898e816a9ae357d
Adam <omahonyadam@gmail.com>
2020-02-19 14:56:57 +0000

    chaneges towards vanilla leaflet

--- frontend/src/Map/Map.js
+++ frontend/src/Map/Map.js
@@ -1,22 +1,32 @@
 import React, {Component} from 'react';
-import { render } from 'react-dom'
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
-import L from 'leaflet'
+import ReactDOM from 'react-dom';
+import { Map, Marker, Popup, Polyline, TileLayer } from 'react-leaflet';
+import L from 'leaflet';
 
 export default function MapComp() {
   const mapbox_url =
    "https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3F1cnJsaSIsImEiOiJjamhxY2lkeTcxOG9jMzdudTZkYm9jbmlkIn0.zvZPOE4XHex2E18F0FSdSg"
 
-  const position = [51.505, -0.09]
+  const map_bounds = [
+    [51.360007, -10.743767],
+    [55.391440, -5.497318]
+  ]
+
+  const lMap = L.map('mapid', {
+    minZoom: 7,
+    bounds: map_bounds,
+    maxBounds: map_bounds
+  })
+
+  const tileLayer = L.TileLayer(mapbox_url).addTo(lMap)
+  
   const map = (
-  <Map center={position} zoom={13}>
+  <Map minZoom={7} bounds={map_bounds} maxBounds={map_bounds}
+       ref={  (mapElm) => this._getMapElement(mapElm) }>
     <TileLayer
       url={mapbox_url}
       attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
     />
-    <Marker position={position}>
-      <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
-    </Marker>
   </Map>
   )
 
@@ -25,4 +35,19 @@ export default function MapComp() {
       { map }
     </div> 
   )
+}
+
+export default class MapComp extends React.Component {
+
+  
+  render() {
+    <div id="map-container">
+      { map }
+    </div> 
+  }
+}
+
+
+function _getMapElement(mapElm) {
+  const polyLine = L.polyline(map_bounds, {color: 'red'}).addTo(mapElm);
 }
