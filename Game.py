6771760b7e05ec30fd5e0db65c82f7c50b94ec0d
JoeTheToe <16163842@studentmail.ul.ie>
2020-02-19 11:38:13 +0000

    more basic classes

--- Game.py
+++ Game.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from Passenger import Passenger
 app = Flask(__name__)
 
 @app.route('/')
 def startGame():
     return 'game started'
 
+@app.route('/hi')
 def endGame():
     return 'game ended'
+
+def generatePassengers():
+    return 0
+
+p = Passenger()
