c38932d7df80a94724267ee6df8c8522dda7afb3
nialldillane <niall.dillane@gmail.com>
2020-02-19 13:34:21 +0000

    generate stops

--- Game.py
+++ Game.py
@@ -8,12 +8,7 @@ app = Flask(__name__)
 
 availableStops = pd.DataFrame()
 usedStops = pd.DataFrame()
-<<<<<<< HEAD
-routes = Tuple[List[Stop],List[Bus]] #((stop1, stop2, stop3), bus1, bus2)
-
-=======
 routes = Tuple[List[Stop], List[Bus]]  # ((stop1, stop2, stop3), bus1, bus2)
->>>>>>> dd1b3695a12b14f76111f6c353ee39a0a8196148
 @app.route('/')
 def startGame():
     loadCSV()
@@ -42,17 +37,9 @@ def generateStops():
     global usedStops
 
     _stop = availableStops.sample(n=1)
-<<<<<<< HEAD
     usedStops = usedStops.append(_stop)
     availableStops = availableStops.drop(_stop.index)
     
     lati = str(_stop.stop_lat.values[0])
     longi = str(_stop.stop_lon.values[0])
     return (lati, longi) # because long is a keyword don't get mad at me
-=======
-    usedStops.append(_stop)
-    availableStops.drop(_stop.index)
-    lati = str(row.stop_lat.values[0])
-    longi = str(row.stop_lon.values[0])
-    return (lati, longi)  # because long is a keyword don't get mad at me
->>>>>>> dd1b3695a12b14f76111f6c353ee39a0a8196148
