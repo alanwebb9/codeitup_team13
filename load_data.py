ffbddf876ed318f4fee22457e59f9b340f12720b
nialldillane <niall.dillane@gmail.com>
2020-02-19 12:02:10 +0000

    loading my dats

--- load_data.py
+++ load_data.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
-df = pd.read_csv('data/stops.txt')
-print(df.head)
+def showData():
+    df = pd.read_csv('data/stops.txt')
+    row = df.sample(n=1)
+    print(row)
+    coords = row.stop_lat
+    print(coords)
 
+showData()
