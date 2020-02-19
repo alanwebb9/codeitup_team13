d31e87584e0fbe451c5292e079f9840ece6ccd96
JoeTheToe <16163842@studentmail.ul.ie>
2020-02-19 11:46:30 +0000

    more boiler plate

--- Game.py
+++ Game.py
@@ -2,8 +2,12 @@ from flask import Flask
 from Passenger import Passenger
 app = Flask(__name__)
 
+availableStops = ()
+unavailableStops = ()
+
 @app.route('/')
 def startGame():
+
     return 'game started'
 
 @app.route('/hi')
@@ -13,4 +17,13 @@ def endGame():
 def generatePassengers():
     return 0
 
-p = Passenger()
+def readCSV():
+    #TODO: populate available stops
+    pass
+
+# this return some kind of
+def generateStops():
+    lati = 0 #TODO load from CSV
+    longi = 0
+    #TODO remove from available stops add to unavailable stops
+    return (lati, longi) # because long is a keyword don't get mad at me
