34cd148fb76310e35ad2d5fed64050e266d30a4e
Neil <neildaly2142@gmail.com>
2020-02-19 13:32:00 +0000

    requirements.in & .txt

--- Game.py
+++ Game.py
@@ -8,28 +8,33 @@ app = Flask(__name__)
 
 availableStops = pd.DataFrame()
 usedStops = pd.DataFrame()
-routes = Tuple[List[Stop],List[Bus]] #((stop1, stop2, stop3), bus1, bus2)
+routes = Tuple[List[Stop], List[Bus]]  # ((stop1, stop2, stop3), bus1, bus2)
 @app.route('/')
 def startGame():
     loadCSV()
     return 'game started'
 
+
 @app.route('/hi')
 def endGame():
     return 'game ended'
 
+
 def generatePassengers():
     return 0
+
 
 @app.route('/loadCSV')
 def loadCSV():
     availableStops = pd.read_csv('data/stops.txt')
 
 # this return some kind of
+
+
 def generateStops():
     _stop = availableStops.sample(n=1)
     usedStops.append(_stop)
     availableStops.drop(_stop.index)
     lati = str(row.stop_lat.values[0])
     longi = str(row.stop_lon.values[0])
-    return (lati, longi) # because long is a keyword don't get mad at me
+    return (lati, longi)  # because long is a keyword don't get mad at me
